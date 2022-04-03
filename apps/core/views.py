1ab9f8c8796a4f4ffc1d972d7aeeb178b5a70e86
joaovitor27 <joaovitor.monteiro.188@gmail.com>
2022-04-03 20:10:49 -0300

    Primeiro commit

---
+++ apps/core/views.py
@@ -0,0 +1,30 @@
+from asyncio.windows_events import NULL
+from django.shortcuts import render
+# Create your views here.
+from django.contrib import messages
+import requests as req
+
+# Create your views here.
+def home(request):
+    template_name = 'base.html'
+
+    search = request.GET.get('search')
+    if search=='':
+        search = 0
+        messages.error(request, 'Digite um Numero!')
+        context = {}
+    else:    
+        bancos = req.get(f'http://localhost:8000/api/books/{search}')
+        if bancos.status_code >= 200 and bancos.status_code <=299:
+            bancos_data = bancos.json()
+            print(bancos_data)
+            cod_compensacao = bancos_data['cod_compensacao']
+            nome_instituicao = bancos_data['nome_instituicao']
+            context = {
+                'cod_compensacao': cod_compensacao,
+                'nome_instituicao': nome_instituicao
+            }
+        else:
+            context = {}
+
+    return render(request, template_name, context)
